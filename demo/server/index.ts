d8037a5bde8c723bf689aada2444bd3bdf0d9609
Ashutosh Dubey <ashutosh.dubey3@concentrix.com>
2026-08-19 12:55:36 +0530

    fix: replace ts-node with tsx, add .js extensions to ESM server imports

--- demo/server/index.ts
+++ demo/server/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { processRoute } from './routes/process';
-import { validateRoute } from './routes/validate';
+import { processRoute } from './routes/process.js';
+import { validateRoute } from './routes/validate.js';
 
 const app = express();
 app.use(cors({ origin: 'http://localhost:5173' }));
